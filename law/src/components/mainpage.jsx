060dfabf79394630029fad9c49c25dcb6d6ef304
PeushYadav <peushyadav07@gmail.com>
2025-06-10 23:01:29 +0530

    changed some fonts

--- law/src/components/mainpage.jsx
+++ law/src/components/mainpage.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import leftMainText from '../assets/left_main_text.svg';
 import centerMain from '../assets/center-mid.png';
+import BackgroundMain from '../assets/backgroundmain.svg';
 export default class mainpage extends Component {
   render() {
     return (
       <div style={{backgroundColor:'#F0ECE8'}} className='w-screen h-screen'>
         <div className='grid gap-4 grid-cols-12 z-10 w-screen  p-10'>
-            
-            <div className=' col-span-4 min-h-100 h-[400px] w-[95%] flex flex-wrap justify-end align-top'>
+
+
+
+        
+            <div className=' col-span-4 min-h-100 h-[400px] w-[95%] flex flex-wrap justify-end align-top z-10'>
                 <div className='m-4'>
                     <img src={leftMainText} alt="Law Lens Logo" className="w-80  " />
                 </div>
@@ -19,7 +23,7 @@ export default class mainpage extends Component {
                 </div>
 
             </div>
-            <div className='  col-span-5 min-h-100 h-[700px] w-[95%] grid'>
+            <div className='  col-span-5 min-h-100 h-[700px] w-[95%] grid z-10'>
                 
                     <div className='w-auto '>
                         <img src={centerMain} alt="Law Lens Logo " className="  w-[100%] h-[100%] mb-50px " />
@@ -31,7 +35,7 @@ export default class mainpage extends Component {
 
                 
             </div>
-            <div className='col-span-3 min-h-100 w-[95%]'>
+            <div className='col-span-3 min-h-100 w-[95%] z-10'>
                 <div className='flex justify-start mt-10'>
                     <p className='text-left m-4 w-[75%] justify-center font-primary'>
                         Tell Us What You Faced.  We'll quickly guide you to the laws that support you. Take control with clear, reliable information—because understanding your legal rights is the first step toward justice, safety, and self-respect.
